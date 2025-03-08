82aaa8799208e223f1ee848b8181a546b0352ae2
iutue <iutue.game@gmail.com>
2025-03-08 16:45:29 +0900

    SlidePanel을 4가지 방향으로 닫을 수 있게 확장
    슬라이드 패널을 탭 시스템에 활용하기 위해서 기존의 Show, Hide 이외에 Next, Prev 방향 추가함

--- Assets/Scripts/Core/GUI/SlidePanel.cs
+++ Assets/Scripts/Core/GUI/SlidePanel.cs
@@ -1,47 +1,100 @@
 using DG.Tweening;
 using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using static SlidePanel;
 
 /// <summary>
 /// 슬라이드해서 열고 닫을 수 있는 패널 GUI
 /// </summary>
 public class SlidePanel : UIBehaviour
 {
-	[SerializeField] float _openDelay = 0f;
-	[SerializeField] float _closeDelay = 0f;
-	[SerializeField] float _duration = 0.5f;
-	[SerializeField] Ease _easeType = Ease.OutExpo;
+	/// <summary>
+	/// 초기화 후 자동으로 애니메이션을 재생하는가
+	/// </summary>
+	[Tooltip("자동으로 From 상태에서 CurrentState 상태로 전환되는 애니메이션을 실행하는가")]
+	[SerializeField]
+	bool _playOnAwake;
+	/// <summary>
+	/// 현재 상태
+	/// </summary>
+	[SerializeField]
+	State _currentState = State.Focus;
+	/// <summary>
+	/// 슬라이드인 설정
+	/// </summary>
+	StateSetting _slideInSetting => _stateSettings[(int)State.Focus];
+	/// <summary>
+	/// 패널이 열려있는가
+	/// </summary>
+	bool _isOpened => _currentState == State.Focus;
+
+	/// <summary>
+	/// 슬라이딩할 패널
+	/// </summary>
+	[Space]
+	[SerializeField]
+	RectTransform _panelToSlide;
+	/// <summary>
+	/// 패널을 슬라이딩할 때 이동 거리의 단위가 될 GUI
+	/// </summary>
+	[SerializeField]
+	RectTransform _boundary;
+
+	/// <summary>
+	/// 스케일링할 패널
+	/// </summary>
+	[Space]
+	[SerializeField]
+	RectTransform _panelToScale;
+
+	/// <summary>
+	/// 불투명도를 조절할 그룹
+	/// </summary>
+	[Space]
+	[SerializeField]
+	CanvasGroup _transparentGroup;
+	/// <summary>
+	/// 슬라이드 아웃시 비활성화할 캔버스
+	/// </summary>
+	[SerializeField]
+	Canvas _canvasToDisable;
 
 	/// <summary>
 	/// 패널의 상태
 	/// </summary>
 	public enum State
 	{
+		#region SlideIn 상태
 		/// <summary>
-		/// 화면에 있음
+		/// 화면에서 보는 중
 		/// </summary>
-		Idle,
+		Focus,
+		#endregion
+		#region SlideOut 상태
 		/// <summary>
-		/// 화면에 나타나는 중
+		/// 화면에 나타나기 전
 		/// </summary>
 		Show,
 		/// <summary>
-		/// 화면에서 사라지는 중
+		/// 화면에서 사라진 후
 		/// </summary>
 		Hide,
 		/// <summary>
-		/// 다음 창으로 넘어가는 중
+		/// 다음으로 넘어감
 		/// </summary>
 		Next,
 		/// <summary>
-		/// 이전 창으로 돌아가는 중
+		/// 이전으로 돌아감
 		/// </summary>
 		Prev
+		#endregion
 	}
+	/// <summary>
+	/// 상태와 이에 대응하는 애니메이션 설정
+	/// </summary>
 	[Serializable]
-	public class StateData
+	public class StateSetting
 	{
 		public State State;
 		/// <summary>
@@ -56,134 +109,76 @@ public class SlidePanel : UIBehaviour
 		/// 재생 스타일
 		/// </summary>
 		public Ease EaseType = Ease.OutExpo;
+
 		/// <summary>
 		/// 슬라이드 방향
 		/// </summary>
 		[Range(0f, 360f)]
-		public float SlideDirection;
+		public float SlideAngle;
 		/// <summary>
 		/// 슬라이드 거리, 경계 크기에 비례함
 		/// </summary>
 		[Range(0f, 1f)]
 		public float SlideDistance = 1f;
 		/// <summary>
+		/// 슬라이드 위치가 고정됐는가
+		/// true면 위치 수동 고정
+		/// false면 위치 자동 계산
+		/// </summary>
+		public bool IsLocked;
+		/// <summary>
+		/// 방향, 거리, 경계로 계산된 슬라이드 위치
+		/// </summary>
+		public Vector3 SlidePosition;
+
+		/// <summary>
 		/// 크기
 		/// </summary>
 		[Range(0f, 2f)]
 		public float Scale = 1f;
+
 		/// <summary>
 		/// 불투명도
 		/// </summary>
 		[Range(0f, 1f)]
 		public float Alpha = 1f;
+
+		/// <summary>
+		/// 주어진 위치를 기준으로 이 상태의 슬라이드 위치 계산
+		/// </summary>
+		public void UpdateSlidePosition(Vector2 position, Vector2 boundarySize)
+		{
+			if (IsLocked)
+			{
+				//이 상태의 위치가 고정됨
+				return;
+			}
+			float radian = SlideAngle * Mathf.Deg2Rad;
+			Vector2 slideVector = new Vector2(Mathf.Cos(radian), Mathf.Sin(radian)) * SlideDistance;
+			SlidePosition = position + slideVector * boundarySize;
+		}
 	}
-	/// <summary>
-	/// 상태에 해당하는 애니메이션 정보
-	/// </summary>
 	[SerializeField]
-	StateData[] _stateDatas = new StateData[0];
-
-	#region Move
-	/// <summary>
-	/// 패널을 슬라이딩할 때 이동 거리의 단위가 될 GUI
-	/// </summary>
-	[SerializeField]
-	RectTransform _boundary;
-	/// <summary>
-	/// 슬라이딩할 패널
-	/// </summary>
-	[Space]
-	[SerializeField]
-	RectTransform _panel;
-	Canvas _canvas;
-	/// <summary>
-	/// 패널이 열려있는가
-	/// </summary>
-	[SerializeField]
-	bool _isOpened;
-	/// <summary>
-	/// 초기화 후 자동으로 애니메이션을 재생하는가
-	/// </summary>
-	[SerializeField]
-	bool _playOnAwake;
-	/// <summary>
-	/// 닫혔을 때 제거할 게임 오브젝트
-	/// </summary>
-	[SerializeField]
-	GameObject _destroyOnClose;
-	/// <summary>
-	/// 패널 위치가 초기화됐는가<br/>
-	/// true면 직렬화된 현재 위치로 고정되고, false면 초기화시 위치로 고정됨
-	/// </summary>
-	[SerializeField]
-	bool _isPositionInitialized;
-	/// <summary>
-	/// 열리기 전 위치
-	/// </summary>
-	Vector2 _openingPosition;
-	/// <summary>
-	/// 열린 후 위치
-	/// </summary>
-	[SerializeField]
-	Vector2 _openedPosition;
-	/// <summary>
-	/// 닫힌 후 위치
-	/// </summary>
-	Vector2 _closedPosition;
-	/// <summary>
-	/// 열릴 때 패널의 이동 방향과 거리
-	/// </summary>
-	[Tooltip(
-		"이동 거리는 경계의 크기에 비례하고 크기가 1이면 경계의 크기만큼 이동한다.\n" +
-		"(열리기 전 위치) = (direction) X (boundary.size)")]
-	[SerializeField]
-	Vector2 _openDirection = Vector2.left;
-	/// <summary>
-	/// 닫힐 때 패널의 이동 방향과 거리
-	/// </summary>
-	[Tooltip(
-		"이동 거리는 경계의 크기에 비례하고 크기가 1이면 경계의 크기만큼 이동한다.\n" +
-		"(닫힌 후 위치) = (direction) X (boundary.size)")]
-	[SerializeField]
-	Vector2 _closeDirection = Vector2.left;
-	#endregion
-
-	#region Alpha
-	/// <summary>
-	/// 불투명도를 조절할 그룹
-	/// </summary>
-	[Space]
-	[SerializeField]
-	CanvasGroup _transparentGroup;
-	/// <summary>
-	/// 열렸을 때 패널의 불투명도
-	/// </summary>
-	[SerializeField, Range(0f, 1f)]
-	float _openedAlpha = 1f;
-	/// <summary>
-	/// 닫혔을 때 패널의 불투명도
-	/// </summary>
-	[SerializeField, Range(0f, 1f)]
-	float _closedAlpha = 0f;
-	#endregion
+	StateSetting[] _stateSettings = new StateSetting[0];
 
 	protected override void OnValidate()
 	{
 		base.OnValidate();
-
+		//
 		Array states = Enum.GetValues(typeof(State));
-		Array.Resize(ref _stateDatas, states.Length);
+		Array.Resize(ref _stateSettings, states.Length);
 		for (int i = 0; i < states.Length; i++)
 		{
-			_stateDatas[i].State = (State)states.GetValue(i);
+			_stateSettings[i] ??= new StateSetting();
+			_stateSettings[i].State = (State)states.GetValue(i);
 		}
 
 		//경계 초기화
 		InitializeBoundary();
-		//위치 초기화
-		if (_panel && !_isPositionInitialized)
+		//현재 위치로 설정
+		if (_panelToSlide && !_slideInSetting.IsLocked)
 		{
-			_openedPosition = _panel.anchoredPosition;
+			_slideInSetting.SlidePosition = _panelToSlide.anchoredPosition;
 		}
 	}
 
@@ -193,71 +188,60 @@ public class SlidePanel : UIBehaviour
 		//경계 초기화
 		InitializeBoundary();
 		//위치 초기화
-		if (_panel && !_isPositionInitialized)
+		if (_panelToSlide && !_slideInSetting.IsLocked)
 		{
-			_isPositionInitialized = true;
-			_openedPosition = _panel.anchoredPosition;
+			//초기 위치 저장
+			_slideInSetting.IsLocked = true;
+			_slideInSetting.SlidePosition = _panelToSlide.anchoredPosition;
 		}
-		CalculatePositions();
+		UpdatePositions();
 		//상태 초기화
 		if (_playOnAwake)
 		{
-			//부드럽게 상태 초기화
-			if (_isOpened)
-			{
-				Open();
-			}
-			else
-			{
-				Close();
-			}
+			ChangeState(State.Show, _currentState);
 		}
 		else
 		{
-			//즉시 상태 초기화
-			if (_panel) _panel.anchoredPosition = _isOpened ? _openedPosition : _closedPosition;
-			if (_transparentGroup) _transparentGroup.alpha = _isOpened ? _openedAlpha : _closedAlpha;
+			SetState(_currentState);
 		}
 	}
 
 	protected override void OnRectTransformDimensionsChange()
 	{
 		base.OnRectTransformDimensionsChange();
-		if (_panel && _isPositionInitialized)
+		if (_panelToSlide && _slideInSetting.IsLocked)
 		{
-			//해상도에 맞는 위치로 즉시 이동
-			CalculatePositions();
-			_panel.anchoredPosition = _isOpened ? _openedPosition : _closedPosition;
+			//변경된 경계에 맞는 상태를 즉시 반영
+			UpdatePositions();
+			SetState(_currentState);
 		}
 	}
 
 	protected void OnDrawGizmosSelected()
 	{
-		if (_panel && _boundary)
+		if (_panelToSlide)
 		{
-			CalculatePositions();
-			//로컬 좌표를 월드 좌표로 변환
-			var parentTr = transform.parent;
-			Vector2 opened = parentTr.TransformPoint(_openedPosition);
-			Vector2 opening = parentTr.TransformPoint(_openingPosition);
-			Vector2 closed = parentTr.TransformPoint(_closedPosition);
-			//열리기 전 위치
-			Gizmos.color = Color.red;
-			Gizmos.DrawWireSphere(opening, 50f);
-			Gizmos.DrawLine(opening, opened);
-			//열린 후 위치
-			Gizmos.color = Color.green;
-			Gizmos.DrawWireSphere(opened, 50f);
-			//닫힌 후 위치
-			Gizmos.color = Color.blue;
-			Gizmos.DrawWireSphere(closed, 50f);
-			Gizmos.DrawLine(opened, closed);
+			UpdatePositions();
+			var tr = transform.parent ? transform.parent : transform;
+			Vector2 focusPos = tr.TransformPoint(_slideInSetting.SlidePosition);
+			Color[] gizmoColors = new Color[] { Color.black, Color.red, Color.magenta, Color.blue, Color.cyan, };
+			for (int i = 0; i < _stateSettings.Length; i++)
+			{
+				//로컬 좌표를 월드 좌표로 변환
+				Vector2 globalPos = tr.TransformPoint(_stateSettings[i].SlidePosition);
+				Gizmos.color = gizmoColors[i];
+				Gizmos.DrawWireSphere(globalPos, 50f);
+				Gizmos.DrawLine(focusPos, globalPos);
+			}
 		}
 	}
 
+	/// <summary>
+	/// 경계 초기화
+	/// </summary>
 	void InitializeBoundary()
 	{
-		if (!_panel)
+		if (!_panelToSlide)
 		{
 			//이동하지 않으면 경계가 필요 없음
 			_boundary = null;
@@ -268,83 +252,100 @@ public class SlidePanel : UIBehaviour
 			//설정된 경계가 이미 있음
 			return;
 		}
-		Canvas canvas = _panel.GetComponentInParent<Canvas>();
-		if (!canvas)
-		{
-			//캔버스 내 UI 엘리먼트가 아님
-			_boundary = null;
-			return;
-		}
+
 		//임의의 경계가 없으면 루트 캔버스(화면)를 경계로 사용
-		_boundary = canvas.rootCanvas.GetComponent<RectTransform>();
+		Canvas canvas = _panelToSlide.GetComponentInParent<Canvas>();
+		if (canvas)
+		{
+			_boundary = canvas.rootCanvas.GetComponent<RectTransform>();
+		}
 	}
 
-	/// <summary>
-	/// 경계의 크기에 비례하는 상태별 패널 위치 계산
-	/// </summary>
-	void CalculatePositions()
+	void UpdatePositions()
 	{
-		Vector2 boundarySize = _boundary.rect.size;
-		_openingPosition = _openedPosition - _openDirection * boundarySize;
-		_closedPosition = _openedPosition + _closeDirection * boundarySize;
+		Vector2 position = _slideInSetting.SlidePosition;
+		Vector2 boundarySize = _boundary ? _boundary.rect.size : Vector2.one;
+		for (int i = 1; i < _stateSettings.Length; i++)
+		{
+			_stateSettings[i].UpdateSlidePosition(position, boundarySize);
+		}
 	}
 
 	/// <summary>
 	/// 패널 슬라이드 인
 	/// </summary>
-	public void Open()
+	public void Open(State from = State.Show)
 	{
-		_isOpened = true;
-		//이동
-		if (_panel)
+		if (_isOpened)
 		{
-			_panel
-				.DOAnchorPos(_openedPosition, _duration)
-				.From(_openingPosition)
-				.SetEase(_easeType)
-				.SetDelay(_openDelay);
+			//이미 열려있음
+			return;
 		}
-		//불투명도
-		if (_transparentGroup)
-		{
-			_transparentGroup
-				.DOFade(_openedAlpha, _duration)
-				.From(_closedAlpha)
-				.SetEase(_easeType)
-				.SetDelay(_openDelay);
-		}
+		ChangeState(from, State.Focus);
 	}
 
 	/// <summary>
 	/// 패널 슬라이드 아웃
 	/// </summary>
-	public void Close()
+	public void Close(State to = State.Hide)
 	{
-		_isOpened = false;
-		//이동
-		if (_panel)
+		if (!_isOpened)
 		{
-			_panel
-				.DOAnchorPos(_closedPosition, _duration)
-				.From(_openedPosition)
-				.SetEase(_easeType)
-				.SetDelay(_closeDelay)
-				.onComplete = () =>
-				{
-					if (_destroyOnClose)
-					{
-						Destroy(_destroyOnClose);
-					}
-				};
+			//이미 닫혀있음
+			return;
+		}
+		ChangeState(State.Focus, to);
+	}
+
+	/// <summary>
+	/// 해당 상태로 설정
+	/// </summary>
+	public void SetState(State state)
+	{
+		_currentState = state;
+		var setting = _stateSettings[(int)state];
+		//위치
+		if (_panelToSlide) _panelToSlide.anchoredPosition = setting.SlidePosition;
+		//크기
+		if (_panelToScale) _panelToScale.localScale = Vector3.one * setting.Scale;
+		//불투명도
+		if (_transparentGroup) _transparentGroup.alpha = setting.Alpha;
+	}
+	/// <summary>
+	/// 이전 상태에서 다음 상태로 부드럽게 전환
+	/// </summary>
+	void ChangeState(State from, State to)
+	{
+		_currentState = to;
+		var fromSetting = _stateSettings[(int)from];
+		var toSetting = _stateSettings[(int)to];
+
+		if (_panelToSlide)
+		{
+			//위치
+			_panelToSlide
+				.DOAnchorPos(toSetting.SlidePosition, toSetting.Duration)
+				.From(fromSetting.SlidePosition)
+				.SetEase(toSetting.EaseType)
+				.SetDelay(toSetting.Delay);
+		}
+		if (_panelToScale)
+		{
+			//크기
+			_panelToScale
+				.DOScale(toSetting.Scale, toSetting.Duration)
+				.From(fromSetting.Scale)
+				.SetEase(toSetting.EaseType)
+				.SetDelay(toSetting.Delay);
 		}
 		if (_transparentGroup)
 		{
 			//불투명도
 			_transparentGroup
-				.DOFade(_closedAlpha, _duration)
-				.From(_openedAlpha)
-				.SetEase(_easeType)
-				.SetDelay(_closeDelay);
+				.DOFade(toSetting.Alpha, toSetting.Duration)
+				.From(fromSetting.Alpha)
+				.SetEase(toSetting.EaseType)
+				.SetDelay(toSetting.Delay);
 		}
 	}
 }
